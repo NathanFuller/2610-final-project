844423ff05b3365e80a53f12c8845bdddaae4d28
Nate Fuller <nathan.fuller@att.net>
2017-04-26 22:31:14 +0000

    Added some functionality to the model

--- forumApp/models.py
+++ forumApp/models.py
@@ -4,8 +4,17 @@ from django.db import models
 
 # Create your models here.
 class Statement(models.Model):
+	"""This is what I'm calling the forum posts"""
 	author = models.CharField(max_length=32)
 	content = models.TextField(max_length=1000)
 	votes = models.IntegerField(default=0)
-	date = models.DateTimeField('date published')
+	date = models.DateTimeField('date of posting')
+	def sortableDate(self):
+		return self.date.strftime('%y%m%d%H%M%S')
+	def __str__(self):
+		if len(self.content)>32:
+			return self.content[:32]+'...'
+		else:
+			return self.content
 
+#note: s.date = datetime.datetime.now()
